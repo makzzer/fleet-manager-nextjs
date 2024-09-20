0546d14bfe455a0937537071b4c97db8e14f5e0a
makzzer <makzofx@gmail.com>
2024-09-20 05:01:51 -0300

    mejor footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,17 +6,7 @@ const Footer = () => {
       <div className="container mx-auto text-center">
         <p className="text-sm">&copy; {new Date().getFullYear()} TrackerAPP.</p>
         <p className="text-sm">Todos los derechos reservados.</p>
-        <ul className="flex justify-center space-x-4 mt-2">
-          <li>
-            <a href="#" className="hover:text-gray-400">Privacidad</a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-gray-400">Términos</a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-gray-400">Contacto</a>
-          </li>
-        </ul>
+
       </div>
     </footer>
   );
