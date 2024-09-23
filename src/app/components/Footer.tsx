abd38b39a22625b0a4138279c82a020f3779eb23
maximiliano sanchez <makzofx@gmail.com>
2024-09-23 04:07:10 -0300

    footer mod 2

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-4">
       <div className="container mx-auto text-center">
-        <p className="text-sm">&copy; {new Date().getFullYear()} Fleet Manager.</p>
+        <p className="text-sm">&copy; {new Date().getFullYear()} Fleet Manager</p>
         <p className="text-sm">Todos los derechos reservados.</p>
 
       </div>
