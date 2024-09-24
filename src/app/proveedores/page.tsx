e2dc7f49566c281684a1222a36295dd3fe018259
Juani <juani.flami@gmail.com>
2024-09-24 01:25:53 -0300

    pagina proveedores oscura

--- src/app/proveedores/page.tsx
+++ src/app/proveedores/page.tsx
@@ -1,16 +1,18 @@
 'use client';
 
 import Link from "next/link";
-import { FaUserTie } from 'react-icons/fa';
+import { FaUserTie, FaEnvelope, FaPhone, FaEnvelopeSquare } from 'react-icons/fa';
 
 const Proveedores = () => {
     return (
-        <div className="min-h-screen bg-gray-100 p-6 text-black">
+        <div className="p-6 min-h-screen bg-gray-900 text-white">
             <h1 className="text-3xl font-bold mb-6">Proveedores</h1>
+
             <Link href="/agregarproveedor"
-                className="flex items-center justify-center bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded shadow-md transition-all duration-300 ease-in-out mb-6">
+                className="flex items-center justify-center bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded shadow-md transition-all duration-300 ease-in-out mb-6">
                 <FaUserTie className="mr-2" /> Agregar Proveedor
             </Link>
+
         </div>
     );
 }
