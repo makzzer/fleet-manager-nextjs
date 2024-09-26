8c9c60dac3b0a39515d7e79dc916a7e0f3b7b3b6
Juani <juani.flami@gmail.com>
2024-09-26 19:02:19 -0300

    array de proveedores locales

--- src/app/context/ProveedorContext.tsx
+++ src/app/context/ProveedorContext.tsx
@@ -31,22 +31,53 @@ export const useProveedor = () => {
   return context;
 };
 
+const proveedoresLocales: Proveedor[] = [
+  {
+    id: "1",
+    name: "Productor 1",
+    cuit: "2022",
+    direccion: 'calle 1',
+    telefono: '100',
+  },
+  {
+    id: "2",
+    name: "Productor 2",
+    cuit: "2023",
+    direccion: "calle 2",
+    telefono: "200",
+  },
+  {
+    id: "3",
+    name: "Productor 3",
+    cuit: "2024",
+    direccion: "calle 3",
+    telefono: "300",
+  }
+]
+
 export const ProveedorProvider = ({ children }: { children: ReactNode }) => {
+  // proveedores de api
+  // const [proveedores, setProveedores] = useState<Proveedor[]>([]);
+
+  // const fetchProveedores = useCallback(async () => {
+  //   try {
+  //     const response = await axios.get(apiProveedoresBackend);
+  //     const fetchedProveedores = response.data;
+
+  //     if (Array.isArray(fetchedProveedores)) {
+  //       setProveedores(fetchedProveedores);
+  //     } else {
+  //       console.error('Error: La respuesta de la API no es un array válido', fetchedProveedores);
+  //     }
+  //   } catch (error) {
+  //     console.error('Error al obtener proveedores:', error);
+  //   }
+  // }, []);
+
   const [proveedores, setProveedores] = useState<Proveedor[]>([]);
 
-  const fetchProveedores = useCallback(async () => {
-    try {
-      const response = await axios.get(apiProveedoresBackend);
-      const fetchedProveedores = response.data;
-
-      if (Array.isArray(fetchedProveedores)) {
-        setProveedores(fetchedProveedores);
-      } else {
-        console.error('Error: La respuesta de la API no es un array válido', fetchedProveedores);
-      }
-    } catch (error) {
-      console.error('Error al obtener proveedores:', error);
-    }
+  const fetchProveedores = useCallback(() => {
+    setProveedores(proveedoresLocales);
   }, []);
 
   const createProveedor = async (proveedor: Omit<Proveedor, 'date_created' | 'date_updated'>) => {
