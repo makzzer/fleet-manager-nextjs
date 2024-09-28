dd05d8bf87cc4e1bec9dcbc60b174fb504624396
maximiliano sanchez <makzofx@gmail.com>
2024-09-27 22:55:10 -0300

    rutas protegidas ok

--- src/app/components/Routes/ProtectedRoutes.tsx
+++ src/app/components/Routes/ProtectedRoutes.tsx
@@ -9,15 +9,20 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-  const { authenticatedUser } = useUser();
+  const { authenticatedUser, isLoading } = useUser();
   const router = useRouter();
 
   useEffect(() => {
-    // Si no hay usuario autenticado, redirigir al login
-    if (!authenticatedUser) {
-      router.push('/login'); // Cambia la ruta al login
+    // Esperar a que termine de cargar el estado del usuario
+    if (!isLoading && !authenticatedUser) {
+      router.push('/login'); // Redirigir al login si no está autenticado
     }
-  }, [authenticatedUser, router]);
+  }, [authenticatedUser, isLoading, router]);
+
+  // Mientras está cargando, puedes mostrar un indicador de carga o nada
+  if (isLoading) {
+    return <div>Cargando...</div>; // Puedes personalizar este mensaje de carga
+  }
 
   // Si hay un usuario autenticado, renderizamos la página protegida
   return authenticatedUser ? <>{children}</> : null;
--- src/app/context/UserContext.tsx
+++ src/app/context/UserContext.tsx
@@ -1,106 +1,94 @@
-'use client';
+'use client'
 
 import React, { createContext, useContext, useState, useEffect, ReactNode, useCallback } from "react";
 import axios from "axios";
-import { useRouter } from "next/router";
 
 // API para autenticación de usuarios
 const apiAuthBackend = `https://fleet-manager-gzui.onrender.com/api/users/auths`;
 
 interface Permissions {
-    module: string;
-    operations: string[];
+  module: string;
+  operations: string[];
 }
 
 interface User {
-    id: string;
-    username: string;
-    full_name: string;
-    roles: string[];
-    permissions: Permissions[];
-    date_created: string;
-    date_updated: string;
+  id: string;
+  username: string;
+  full_name: string;
+  roles: string[];
+  permissions: Permissions[];
+  date_created: string;
+  date_updated: string;
 }
 
 interface AuthResponse {
-    user?: User;
-    error?: string;
+  user?: User;
+  error?: string;
 }
 
 interface UserContextProps {
-    authenticatedUser: User | null;
-    authenticateUser: (username: string, password: string) => Promise<AuthResponse>;
-    logoutUser: () => void;
+  authenticatedUser: User | null;
+  isLoading: boolean; // Añadimos el estado de carga
+  authenticateUser: (username: string, password: string) => Promise<AuthResponse>;
+  logoutUser: () => void;
 }
 
 // Creamos el UserContext
 const UserContext = createContext<UserContextProps | undefined>(undefined);
 
 export const useUser = () => {
-
-
-    const context = useContext(UserContext);
-    if (!context) {
-        throw new Error('useUser debe ser usado dentro de UserProvider');
-    }
-    return context;
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('useUser debe ser usado dentro de UserProvider');
+  }
+  return context;
 };
-
 
 // Provider para envolver la aplicación
 export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [authenticatedUser, setAuthenticatedUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true); // Estado de carga
 
+  // Cargar usuario desde localStorage cuando la aplicación se monta
+  useEffect(() => {
+    const storedUser = localStorage.getItem('authenticatedUser');
+    if (storedUser) {
+      setAuthenticatedUser(JSON.parse(storedUser));
+    }
+    setIsLoading(false); // Terminamos de cargar
+  }, []);
 
-    const [authenticatedUser, setAuthenticatedUser] = useState<User | null>(null);
+  // Función para autenticar al usuario
+  const authenticateUser = useCallback(async (username: string, password: string): Promise<AuthResponse> => {
+    try {
+      const response = await axios.post(apiAuthBackend, { username, password });
+      
+      const user = response.data; // Aquí recibes el objeto User si es exitoso
+      setAuthenticatedUser(user); // Guardamos el usuario autenticado
+      localStorage.setItem('authenticatedUser', JSON.stringify(user)); // Guardar usuario en localStorage
+      return { user }; // Devolvemos el usuario en caso de éxito
 
-    // Función para guardar el usuario en localStorage
-    const saveUserToLocalStorage = (user: User | null) => {
-        if (user) {
-            localStorage.setItem('authenticatedUser', JSON.stringify(user));
-        } else {
-            localStorage.removeItem('authenticatedUser');
-        }
-    };
+    } catch (error) {
+      // Manejar error (e.g., credenciales incorrectas)
+      console.error('Error authenticating user:', error);
+      return { error: 'Usuario o contraseña incorrectos' }; // Mensaje de error
+    }
+  }, []);
 
-    // Recuperar usuario desde localStorage si existe
-    useEffect(() => {
-        const storedUser = localStorage.getItem('authenticatedUser');
-        if (storedUser) {
-            setAuthenticatedUser(JSON.parse(storedUser));
-        }
-    }, []);
+  // Función para cerrar sesión
+  const logoutUser = useCallback(() => {
+    setAuthenticatedUser(null); // Reseteamos el usuario autenticado a null
+    localStorage.removeItem('authenticatedUser'); // Remover usuario de localStorage
+  }, []);
 
-    // Función para autenticar al usuario
-    const authenticateUser = useCallback(async (username: string, password: string): Promise<AuthResponse> => {
-        try {
-            const response = await axios.post(apiAuthBackend, { username, password });
-
-            const user = response.data; // Aquí recibes el objeto User si es exitoso
-            setAuthenticatedUser(user); // Guardamos el usuario autenticado en el estado
-            saveUserToLocalStorage(user); // Guardamos el usuario en localStorage
-            return { user }; // Devolvemos el usuario en caso de éxito
-
-
-        } catch (error) {
-            // Manejar error (e.g., credenciales incorrectas)
-            console.error('Error authenticating user:', error);
-            return { error: 'Usuario o contraseña incorrectos' }; // Mensaje de error
-        }
-    }, []);
-
-    // Función para cerrar sesión
-    const logoutUser = useCallback(() => {
-        setAuthenticatedUser(null); // Reseteamos el usuario autenticado a null
-        saveUserToLocalStorage(null); // Eliminamos el usuario de localStorage
-    }, []);
-
-    return (
-        <UserContext.Provider value={{
-            authenticatedUser,
-            authenticateUser,
-            logoutUser,
-        }}>
-            {children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={{
+      authenticatedUser,
+      isLoading, // Pasamos el estado de carga
+      authenticateUser,
+      logoutUser,
+    }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
