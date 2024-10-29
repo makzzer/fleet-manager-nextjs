6cb980ef9e921eef1a50bcff433c310b5d373272
Matias Leandro <matiasleandroheredia@gmail.com>
2024-10-29 02:44:39 -0300

    Add - Vista de empresas base

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import { VehiculoProvider } from "./context/VehiculoContext";
 import { ProveedorProvider } from "./context/ProveedorContext";
@@ -10,29 +10,30 @@ import { AnalyticsProvider } from "./context/AnalyticsContext";
 import { ControlProvider } from "./context/ControlContext";
 import { ReservaProvider } from "./context/ReservesContext";
 import { AlertProvider } from "./context/AlertsContext";
+import { EnterpriseProvider } from "./context/EnterpriseContext";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <AnalyticsProvider>
-      <ControlProvider>
-        <OrdenDeCompraProvider>
-          <ProductoProvider>
-            <ProveedorProvider>
-              <VehiculoProvider>
-                <AuthProvider>
-                  <ReservaProvider>
-                    <UserProvider>
-                      <AlertProvider>
-                        {children}
-                      </AlertProvider>
-                    </UserProvider>
-                  </ReservaProvider>
-                </AuthProvider>
-              </VehiculoProvider>
-            </ProveedorProvider>
-          </ProductoProvider>
-        </OrdenDeCompraProvider>
-      </ControlProvider>
+      <EnterpriseProvider>
+        <ControlProvider>
+          <OrdenDeCompraProvider>
+            <ProductoProvider>
+              <ProveedorProvider>
+                <VehiculoProvider>
+                  <AuthProvider>
+                    <ReservaProvider>
+                      <UserProvider>
+                        <AlertProvider>{children}</AlertProvider>
+                      </UserProvider>
+                    </ReservaProvider>
+                  </AuthProvider>
+                </VehiculoProvider>
+              </ProveedorProvider>
+            </ProductoProvider>
+          </OrdenDeCompraProvider>
+        </ControlProvider>
+      </EnterpriseProvider>
     </AnalyticsProvider>
   );
 };
