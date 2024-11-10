a3733376cf13f61335c93cab0b55322d05dfbf55
LeonelaS <leonelafigueroa163@gmail.com>
2024-11-09 23:09:18 -0300

    add-adicion opciones de carga proveedor

--- src/app/proveedores/page.tsx
+++ src/app/proveedores/page.tsx
@@ -7,22 +7,29 @@ import Swal from "sweetalert2";
 import ProveedorCard from "../components/Cards/ProveedorCards";
 import { useProveedor } from "../context/ProveedorContext";
 import { FaDownload, FaPlusCircle } from "react-icons/fa";
-import { TextField, InputAdornment } from '@mui/material';
-import { FaSearch } from 'react-icons/fa';
+import { TextField, InputAdornment } from "@mui/material";
+import { FaSearch } from "react-icons/fa";
 
 interface Proveedor {
-  id: string,
-  name: string,
-  email: string,
-  cuit: string,
-  phone_number: string,
-  address: string
+  id: string;
+  name: string;
+  email: string;
+  cuit: string;
+  phone_number: string;
+  address: string;
 }
 
 const Proveedores = () => {
-  const { proveedores, fetchProveedores, createProveedor, exportProveedorToExcel } = useProveedor();
+  const {
+    proveedores,
+    fetchProveedores,
+    createProveedor,
+    exportProveedorToExcel,
+  } = useProveedor();
   const [isLoading, setIsLoading] = useState(true);
-  const [filteredProveedores, setFilteredProveedores] = useState<Proveedor[]>([]);
+  const [filteredProveedores, setFilteredProveedores] = useState<Proveedor[]>(
+    []
+  );
   const [loadMoreCount, setLoadMoreCount] = useState(6); // Para cargar de a 6
   const [nameSearchTerm, setNameSearchTerm] = useState("");
   const [emailSearchTerm, setEmailSearchTerm] = useState("");
@@ -46,22 +53,24 @@ const Proveedores = () => {
 
     // Filtrado por nombre
     if (nameSearchTerm) {
-      filtered = filtered.filter(proveedor =>
+      filtered = filtered.filter((proveedor) =>
         proveedor.name.toLowerCase().includes(nameSearchTerm.toLowerCase())
       );
     }
 
     // Filtrado por email
     if (emailSearchTerm) {
-      filtered = filtered.filter(proveedor =>
+      filtered = filtered.filter((proveedor) =>
         proveedor.email.toLowerCase().includes(emailSearchTerm.toLowerCase())
       );
     }
 
     // Filtrado por dirección
     if (addressSearchTerm) {
-      filtered = filtered.filter(proveedor =>
-        proveedor.address.toLowerCase().includes(addressSearchTerm.toLowerCase())
+      filtered = filtered.filter((proveedor) =>
+        proveedor.address
+          .toLowerCase()
+          .includes(addressSearchTerm.toLowerCase())
       );
     }
 
@@ -70,6 +79,49 @@ const Proveedores = () => {
 
   const handleLoadMore = () => {
     setLoadMoreCount(loadMoreCount + 6); // Cargar 6 proveedores más
+  };
+
+  const handleDesplegarOpciones = () => {
+    Swal.fire({
+      //libreria s. alert
+      title: "Agregar Proveedor",
+      html: `
+      <div class="grid grid-cols-2">
+        <button id="addIndividual" class="swal2-confirm swal2-styled flex flex-col justify-center items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
+          </svg>
+          <span>Individual</span>
+        </button>
+        <button id="addMasivo" class="swal2-confirm swal2-styled flex flex-col justify-center items-center gap-1">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+        </svg>
+        <span>Carga Masiva</span>
+        </button>
+      </div> 
+      `,
+      showConfirmButton: false,
+      showCancelButton: true,
+      cancelButtonText: "Cancelar",
+      focusConfirm: false,
+      customClass: {
+        title: "text-white",
+        popup: "bg-gray-800",
+      },
+      didOpen: () => {
+        document
+          .getElementById("addIndividual")
+          ?.addEventListener("click", () => {
+            Swal.close();
+            handleAgregarProveedor();
+          });
+        document.getElementById("addMasivo")?.addEventListener("click", () => {
+          Swal.close();
+          // handleCargaMasiva();
+        });
+      },
+    });
   };
 
   const handleAgregarProveedor = () => {
@@ -88,12 +140,22 @@ const Proveedores = () => {
       showCancelButton: true,
       preConfirm: async () => {
         const nameElement = document.getElementById("name") as HTMLInputElement;
-        const emailElement = document.getElementById("email") as HTMLInputElement;
+        const emailElement = document.getElementById(
+          "email"
+        ) as HTMLInputElement;
         const cuitElement = document.getElementById("cuit") as HTMLInputElement;
-        const phone_numberElement = document.getElementById("phone_number") as HTMLInputElement;
-        const streetElement = document.getElementById("street") as HTMLInputElement;
-        const numberElement = document.getElementById("number") as HTMLInputElement;
-        const localityElement = document.getElementById("locality") as HTMLInputElement;
+        const phone_numberElement = document.getElementById(
+          "phone_number"
+        ) as HTMLInputElement;
+        const streetElement = document.getElementById(
+          "street"
+        ) as HTMLInputElement;
+        const numberElement = document.getElementById(
+          "number"
+        ) as HTMLInputElement;
+        const localityElement = document.getElementById(
+          "locality"
+        ) as HTMLInputElement;
 
         const name = nameElement?.value;
         const email = emailElement?.value;
@@ -112,22 +174,30 @@ const Proveedores = () => {
         const localityRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/; // Regex para la localidad
 
         if (!name || !nameRegex.test(name)) {
-          Swal.showValidationMessage("Nombre inválido. El nombre no puede estar vacío ni contener caracteres especiales o números.");
+          Swal.showValidationMessage(
+            "Nombre inválido. El nombre no puede estar vacío ni contener caracteres especiales o números."
+          );
           return null;
         }
 
         if (!email || !emailRegex.test(email)) {
-          Swal.showValidationMessage("Email inválido. El email no puede estar vacío, además debe contener un solo '@' y un formato válido.");
+          Swal.showValidationMessage(
+            "Email inválido. El email no puede estar vacío, además debe contener un solo '@' y un formato válido."
+          );
           return null;
         }
 
         if (!cuit || !cuitRegex.test(cuit)) {
-          Swal.showValidationMessage("CUIT inválido. Debe seguir el formato 00-00000000-0.");
+          Swal.showValidationMessage(
+            "CUIT inválido. Debe seguir el formato 00-00000000-0."
+          );
           return null;
         }
 
         if (!phone_number || !telefonoRegex.test(phone_number)) {
-          Swal.showValidationMessage("Teléfono inválido. Debe contener solo números y opcionalmente guiones.");
+          Swal.showValidationMessage(
+            "Teléfono inválido. Debe contener solo números y opcionalmente guiones."
+          );
           return null;
         }
 
@@ -135,18 +205,24 @@ const Proveedores = () => {
           Swal.showValidationMessage("Calle inválida.");
           return null;
         }
-  
+
         if (!number || !numberRegex.test(number)) {
           Swal.showValidationMessage("Número inválido.");
           return null;
         }
-  
+
         if (!locality || !localityRegex.test(locality)) {
           Swal.showValidationMessage("Localidad inválida.");
           return null;
         }
 
-        return { name, email, cuit, phone_number, address: `${street} ${number}, ${locality}`, };
+        return {
+          name,
+          email,
+          cuit,
+          phone_number,
+          address: `${street} ${number}, ${locality}`,
+        };
       },
     }).then(async (result) => {
       if (result.isConfirmed && result.value) {
@@ -163,7 +239,12 @@ const Proveedores = () => {
           await createProveedor(proveedor);
           Swal.fire("Proveedor agregado con éxito", "", "success");
         } catch (error) {
-          Swal.fire("Error", "Hubo un problema al agregar el proveedor", "error");
+          console.error(error)
+          Swal.fire(
+            "Error",
+            "Hubo un problema al agregar el proveedor",
+            "error"
+          );
         }
       }
     });
@@ -178,7 +259,7 @@ const Proveedores = () => {
           </h1>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <button
-              onClick={handleAgregarProveedor}
+              onClick={handleDesplegarOpciones}
               className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded shadow-md transition-all duration-300 ease-in-out flex items-center justify-center"
             >
               <FaPlusCircle className="mr-2" /> Agregar Proveedor
@@ -195,64 +276,64 @@ const Proveedores = () => {
         {/* Filtros independientes */}
         <div className="flex flex-col md:flex-row gap-6 mb-6">
           <TextField
-                label="Filtrar por nombre"
-                variant="outlined"
-                value={nameSearchTerm}
-                onChange={(e) => setNameSearchTerm(e.target.value)}
-                fullWidth
-                className="bg-gray-800 text-white rounded-lg shadow-md border border-gray-600 transition-all duration-300 ease-in-out hover:shadow-lg focus:shadow-lg"
-                InputProps={{
-                    style: { color: "#fff" },
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <FaSearch className="text-gray-300" />
-                        </InputAdornment>
-                    ),
-                }}
-                InputLabelProps={{
-                    style: { color: "#b0b0b0" },
-                }}
-             /> 
-           <TextField
-                label="Filtrar por Email"
-                variant="outlined"
-                value={emailSearchTerm}
-                onChange={(e) => setEmailSearchTerm(e.target.value)}
-                fullWidth
-                className="bg-gray-800 text-white rounded-lg shadow-md border border-gray-600 transition-all duration-300 ease-in-out hover:shadow-lg focus:shadow-lg"
-                InputProps={{
-                    style: { color: "#fff" },
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <FaSearch className="text-gray-300" />
-                        </InputAdornment>
-                    ),
-                }}
-                InputLabelProps={{
-                    style: { color: "#b0b0b0" },
-                }}
-             />     
-            <TextField
-                label="Filtrar por Dirección"
-                variant="outlined"
-                value={addressSearchTerm}
-                onChange={(e) => setAddressSearchTerm(e.target.value)}
-                fullWidth
-                className="bg-gray-800 text-white rounded-lg shadow-md border border-gray-600 transition-all duration-300 ease-in-out hover:shadow-lg focus:shadow-lg"
-                InputProps={{
-                    style: { color: "#fff" },
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <FaSearch className="text-gray-300" />
-                        </InputAdornment>
-                    ),
-                }}
-                InputLabelProps={{
-                    style: { color: "#b0b0b0" },
-                }}
-              />     
-         </div>
-      
+            label="Filtrar por nombre"
+            variant="outlined"
+            value={nameSearchTerm}
+            onChange={(e) => setNameSearchTerm(e.target.value)}
+            fullWidth
+            className="bg-gray-800 text-white rounded-lg shadow-md border border-gray-600 transition-all duration-300 ease-in-out hover:shadow-lg focus:shadow-lg"
+            InputProps={{
+              style: { color: "#fff" },
+              startAdornment: (
+                <InputAdornment position="start">
+                  <FaSearch className="text-gray-300" />
+                </InputAdornment>
+              ),
+            }}
+            InputLabelProps={{
+              style: { color: "#b0b0b0" },
+            }}
+          />
+          <TextField
+            label="Filtrar por Email"
+            variant="outlined"
+            value={emailSearchTerm}
+            onChange={(e) => setEmailSearchTerm(e.target.value)}
+            fullWidth
+            className="bg-gray-800 text-white rounded-lg shadow-md border border-gray-600 transition-all duration-300 ease-in-out hover:shadow-lg focus:shadow-lg"
+            InputProps={{
+              style: { color: "#fff" },
+              startAdornment: (
+                <InputAdornment position="start">
+                  <FaSearch className="text-gray-300" />
+                </InputAdornment>
+              ),
+            }}
+            InputLabelProps={{
+              style: { color: "#b0b0b0" },
+            }}
+          />
+          <TextField
+            label="Filtrar por Dirección"
+            variant="outlined"
+            value={addressSearchTerm}
+            onChange={(e) => setAddressSearchTerm(e.target.value)}
+            fullWidth
+            className="bg-gray-800 text-white rounded-lg shadow-md border border-gray-600 transition-all duration-300 ease-in-out hover:shadow-lg focus:shadow-lg"
+            InputProps={{
+              style: { color: "#fff" },
+              startAdornment: (
+                <InputAdornment position="start">
+                  <FaSearch className="text-gray-300" />
+                </InputAdornment>
+              ),
+            }}
+            InputLabelProps={{
+              style: { color: "#b0b0b0" },
+            }}
+          />
+        </div>
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
           {isLoading ? (
             Array(6)
