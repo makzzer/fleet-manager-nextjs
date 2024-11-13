6c883c6b28388abbe781b8869f98baf4ee457d3b
Matias Leandro <matiasleandroheredia@gmail.com>
2024-11-13 16:56:17 -0300

    Add - Primera adaptación de estilos a la card del nuevo kanban

--- src/app/components/KanbanBoardV2/Card.tsx
+++ src/app/components/KanbanBoardV2/Card.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import { DropIndicator } from './DropIndicator'
 import { Control } from '@/app/context/ControlContext'
+import { FaRegCalendarAlt, FaUserCircle } from 'react-icons/fa'
+import { FiAlertCircle, FiTool, FiTruck } from 'react-icons/fi'
+import { IoIosArrowDown, IoIosArrowUp, IoIosRemove } from 'react-icons/io'
+import { IoPulse } from 'react-icons/io5'
+import { useRouter } from 'next/navigation'
 
 interface CardProps {
   control: Control
@@ -10,6 +15,51 @@ interface CardProps {
 }
 
 export const Card: React.FC<CardProps> = ({ control, handleDragStart }) => {
+
+  const router = useRouter()
+
+  const priorityLogo = (priority: string) => {
+    switch (priority) {
+      case "HIGH":
+        return <IoIosArrowUp className="w-6 h-6 text-red-500" />
+      case "MEDIUM":
+        return <IoIosRemove className="w-6 h-6 text-yellow-500" />
+      default:
+        return <IoIosArrowDown className="w-6 h-6 text-blue-500" />
+    }
+  }
+
+  const typeLogo = (type: string) => {
+    const typeStyles = "flex inline-flex items-center gap-2 px-3 py-1 text-xs font-semibold rounded-full mb-2 "
+    switch (type) {
+      case "CORRECTIVE":
+        return (
+          <div className={`${typeStyles} bg-red-500 text-white`}>
+            <FiTool />
+            <span>Correctivo</span>
+          </div>
+        )
+      case "PREVENTIVE":
+        return (
+          <div className={`${typeStyles} bg-green-500 text-white`}>
+            <FiAlertCircle />
+            <span>Preventivo</span>
+          </div>
+        )
+      default:
+        return (
+          <div className={`${typeStyles} bg-blue-500 text-white`}>
+            <IoPulse />
+            <span>Predictivo</span>
+          </div>
+        )
+    }
+  }
+
+  const handleViewDetails = () => {
+    router.push(`/control/${control.id}`)
+  }
+
   return (
     <>
       <DropIndicator beforeId={control.id} column={control.status} />
@@ -18,11 +68,25 @@ export const Card: React.FC<CardProps> = ({ control, handleDragStart }) => {
         layoutId={control.id}
         draggable="true"
         onDragStart={(e) => handleDragStart(e, control)}
+        onClick={handleViewDetails}
         className="cursor-grab rounded border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing"
       >
-        <p className="text-sm text-neutral-100">{control.subject}</p>
-        <p className="text-xs text-neutral-400">{control.vehicle.brand} {control.vehicle.model}</p>
-        <p className="text-xs text-neutral-400">{control.type}</p>
+        <div className="flex justify-between items-start mb-2">
+        <h3 className="text-lg font-semibold text-white truncate">{control.subject}</h3>
+        {priorityLogo(control.priority)}
+      </div>
+      {typeLogo(control.type)}
+      <div className="flex items-center gap-2 text-gray-400 mb-2">
+        <FiTruck />
+        <span>{control.vehicle.id}</span>
+      </div>
+      <div className="flex justify-between items-center text-sm text-gray-500 pt-2 border-t border-gray-700">
+        <div className="flex items-center gap-2">
+          <FaRegCalendarAlt />
+          <span>{new Date(control.date_created).toLocaleDateString()}</span>
+        </div>
+        {control.operator && <FaUserCircle className="w-5 h-5" />}
+      </div>
       </motion.div>
     </>
   )
